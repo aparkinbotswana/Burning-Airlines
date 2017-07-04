ae9cadf3562584ba4cc66f5f664c3b3d6744d83b
“juliantreweeke” <juliantreweeke@gmail.com>
2017-07-04 19:41:25 +1000

    fixed is_admin boolean in table

--- Burning-Airlines/db/schema.rb
+++ Burning-Airlines/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170704071827) do
+ActiveRecord::Schema.define(version: 20170704093104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,7 +47,7 @@ ActiveRecord::Schema.define(version: 20170704071827) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.string   "is_admin"
+    t.boolean  "is_admin"
     t.string   "false"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
