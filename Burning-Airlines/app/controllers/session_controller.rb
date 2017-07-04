19d6086129660d95fda80b687acc9a7f5b00e051
“juliantreweeke” <juliantreweeke@gmail.com>
2017-07-04 20:45:28 +1000

    Working on login page, password authentication not working

--- Burning-Airlines/app/controllers/session_controller.rb
+++ Burning-Airlines/app/controllers/session_controller.rb
@@ -5,20 +5,25 @@ class SessionController < ApplicationController
 
   def create
 
+
+
     user = User.find_by email: params[:email]
+    redirect_to user_path(user)
+    # redirect_to user_path(user)
 
-    if user.present? and user.authenticate params[:password]
-      # if email is in the database and the password is correct
+    # if user.present? and user.authenticate params[:password]
+    #
+    #   # new julian code -- not working
+    #
+    #   session[:user_id] = user.id # set the cookie to the user id
+    #   redirect_to user_path(user) # redirect to user profile
+    # else
+    #   #mitmatch/bad credentials
+    #   flash[:error] = "Incorrect email address and/or password"
+    #   redirect_to login_path
+    # end
 
-      # success
-      session[:user_id] = user.id # set the cookie to the user id
-      redirect_to user_path(user) # redirect to user profile
-    else
-      #mitmatch/bad credentials
-      flash[:error] = "Incorrect email address and/or password"
-      redirect_to login_path
-    end
-
+    # end new julian code
 
   end
 
