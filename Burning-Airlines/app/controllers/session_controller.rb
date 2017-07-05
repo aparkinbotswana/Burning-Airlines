ad7fc1e0ad28bfc8b63feba095994216e671b3b7
“juliantreweeke” <juliantreweeke@gmail.com>
2017-07-05 10:01:53 +1000

    worked on user authentication and fixed seeding

--- Burning-Airlines/app/controllers/session_controller.rb
+++ Burning-Airlines/app/controllers/session_controller.rb
@@ -8,20 +8,26 @@ class SessionController < ApplicationController
 
 
     user = User.find_by email: params[:email]
-    redirect_to user_path(user)
+
+    #
+    #
+    # if user.admin
+    #
+    # redirect_to user_path(user)
+    #
     # redirect_to user_path(user)
 
-    # if user.present? and user.authenticate params[:password]
-    #
-    #   # new julian code -- not working
-    #
-    #   session[:user_id] = user.id # set the cookie to the user id
-    #   redirect_to user_path(user) # redirect to user profile
-    # else
-    #   #mitmatch/bad credentials
-    #   flash[:error] = "Incorrect email address and/or password"
-    #   redirect_to login_path
-    # end
+    if user.present? and user.authenticate params[:password]
+
+      # new julian code -- not working
+
+      session[:user_id] = user.id # set the cookie to the user id
+      redirect_to user_path(user) # redirect to user profile
+    else
+      #mitmatch/bad credentials
+      flash[:error] = "Incorrect email address and/or password"
+      redirect_to login_path
+    end
 
     # end new julian code
 
