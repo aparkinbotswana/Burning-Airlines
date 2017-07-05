cc7e3aab838acf518455c27992305d5ddb2f244e
Unicar <Unicar@Macintosh.lan>
2017-07-05 20:55:15 +1000

    render json

--- Burning-Airlines/app/assets/javascripts/application.js
+++ Burning-Airlines/app/assets/javascripts/application.js
@@ -12,9 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require semantic-ui
 //= require underscore
 //= require backbone
-//= require semantic-ui
 //= require_tree ./models
 //= require_tree ./collections
 //= require_tree ./views
