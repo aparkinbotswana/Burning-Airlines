03fda845637a865c4b611e660abdc13f18326870
Unicar <Unicar@Macintosh.local>
2017-07-05 16:13:27 +1000

    search page fixed

--- Burning-Airlines/app/assets/javascripts/views/Appview.js
+++ Burning-Airlines/app/assets/javascripts/views/Appview.js
@@ -1,7 +1,33 @@
 app.AppView = Backbone.View.extend({
   el: "#app",
 
-  render:  function() {
+  events: {
+    'click button': 'searchFlights'
+  },
+
+  searchFlights: function() {
+    console.log('worked');
+    var $from = $('#from').val();
+    var $to = $('#to').val();
+    console.log($from, $to);
+
+    var results = _.filter(app.flights.models, function(flight) {
+      return flight.get('from') === $from && flight.get('to') === $to;
+    });
+
+    console.log(results.attributes);
+
+    _.each(results, function(flight) {
+      var sv = new app.SearchFlightView({
+        model: flight
+      });
+      sv.render();
+    });
+
+
+  },
+
+  render: function() {
 
     var template = $('#AppViewTemplate').html();
     this.$el.html( template );
@@ -15,9 +41,6 @@ app.AppView = Backbone.View.extend({
       flv.render();
 
     });
-
-
-
 
   }
 
