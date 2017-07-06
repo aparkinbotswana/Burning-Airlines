cc14a4ce55749aa081259121204dca6fff2b853e
“juliantreweeke” <juliantreweeke@gmail.com>
2017-07-06 18:26:19 +1000

    thursday night push

--- Burning-Airlines/app/assets/javascripts/views/ReservationView.js
+++ Burning-Airlines/app/assets/javascripts/views/ReservationView.js
@@ -2,16 +2,26 @@ var app = app || {};
 
 app.ReservationView = Backbone.View.extend({
 
-  el: "#app",
+  tagName: "div",
 
 
   render: function () {
     var rawTemplate = $('#ReservationViewTemplate').html();
     var template = _.template( rawTemplate);
 
-    var markup = template( this.model.attributes );
 
+
+    var markup = template( this.model.attributes );
     this.$el.html( markup );
+    this.$el.appendTo( "#reserved" );
+
+
+
+
+
+
+
+
 
   }
 
